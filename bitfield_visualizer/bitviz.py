1ba3e9ae2af29881a3337fa3972d8f53eca60ae9
Jon Palmisciano <jp@jonpalmisc.com>
2023-01-10 14:07:13 -0500

    bitfield_visualizer: Move library code into separate file

--- bitfield_visualizer/bitviz.py
+++ bitfield_visualizer/bitviz.py
@@ -1,6 +1,3 @@
-#!/usr/bin/env python3
-
-
 UNICODE_ARROW = "\u25B2"
 UNICODE_VLINE = "\u2502"
 UNICODE_HLINE = "\u2500"
@@ -98,32 +95,3 @@ class Bitfield:
             print("")
 
         print("")
-
-
-upper = Bitfield(
-    [
-        Field("GP", 50),
-        Field("DBM", 51),
-        Field("Contiguous", 52),
-        Field("PXN", 53),
-        Field("(U)XN", 54),
-        Field("Reserved", 58, 55),
-        Field("PBHA", 62, 59),
-        Field("Ignored", 63),
-    ]
-)
-lower = Bitfield(
-    [
-        Field("AttrIndex[2:0]", 4, 2),
-        Field("NS", 5),
-        Field("AP[2:1]", 7, 6),
-        Field("SH[1:0]", 9, 8),
-        Field("AF", 10),
-        Field("nG", 11),
-        Field("OA", 15, 12),
-        Field("nT", 16),
-    ]
-)
-
-upper.diagram(0x60000180000625)
-lower.diagram(0x60000180000625)
